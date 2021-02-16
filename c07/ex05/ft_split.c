310b6213451c3c8a26dc418bcabe206833d43806
Stone <stone@pop-os.localdomain>
2021-02-16 21:27:11 +0100

    the main work is done in ft_split.c, need to fix norm error

--- c07/ex05/ft_split.c
+++ c07/ex05/ft_split.c
@@ -6,14 +6,14 @@
 /*   By: sdummett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 12:37:23 by sdummett          #+#    #+#             */
-/*   Updated: 2021/02/16 17:18:18 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/02/16 21:24:31 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
 	int len;
 
@@ -23,11 +23,68 @@ int ft_strlen(char *str)
 	return (len);
 }
 
-void ft_sort_tab(int *tab, int size)
+int	ft_check_charset(char c, char *charset)
 {
 	int i;
+
+	i = 0;
+	while (charset[i] != 0)
+	{
+		if (c == charset[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_create_tab(char *str, char *charset, int *tab)
+{
+	int i;
+	int j;
+	int size;
 	int temp;
 
+	i = 0;
+	size = 0;
+	//THIS CODE WORK
+//	while (charset[i] != 0)
+//	{
+		//j = 0;
+	//	while (str[j] != 0)
+	//	{
+	//		if (str[j] == charset[i])
+	//		{
+	//			tab[size] = j;
+	//			size++;
+	//		}
+	//		j++;
+	//	}
+		//i++;
+		//if (charset[i] == 0 && str[j] != 0)
+		//	i = 0;
+//	}
+//------------------------------------------------------------------------//
+	char c;
+	c = charset[0];
+	c++;
+	j = 0;
+	while (str[j] != 0)
+	{
+		if (ft_check_charset(str[j], charset))
+		{
+			while (ft_check_charset(str[j], charset) && str[j] != 0)
+			{
+				tab[size] = j;
+				j++;
+				size++;
+			}
+		}
+		else
+		{
+			j++;
+		}
+	}
+//------------------------------------------------------------------------//
 	i = 0;
 	while (i < size - 1)
 	{
@@ -38,73 +95,99 @@ void ft_sort_tab(int *tab, int size)
 			tab[i + 1] = temp;
 			i = 0;
 		}
+		else
+			i++;
+	}
+	i = 0;
+	while (i < size)
+	{
+		printf("tab[%d]%d\n", i, tab[i]);
 		i++;
 	}
+	return (size);
 }
 
-int **ft_split(char *str, char *charset)
+int	ft_count_strings(char *str, char *charset)
 {
 	int i;
 	int j;
-	int k;
 	int len;
 	int *tab;
 
 	tab = (int *)malloc(sizeof(int) * ft_strlen(str));
+	j = ft_create_tab(str, charset, tab);
 	i = 0;
-	k = 0;
-	len = 0;
-	while (charset[i] != 0)
+	if (tab[0] == 0)
 	{
-		j = 0;
-		while (str[j] != 0)
-		{
-			if (str[j] == charset[i])
-			{
-				tab[k] = j;
-				k++;
-			}
-			j++;
-		}
-		i++;
-		if ( charset[i] == 0 && str[j] != 0)
-			i = 0;
-	}
-	ft_sort_tab(tab, k);
-	i = 0;
-	while (i < k)
-	{
-		printf("tab[%d]:%d\n", i, tab[i]);
+		i = 0;
+		while (tab[i] == tab[i + 1] - 1)
+			i++;
 		i++;
 	}
- 	i = 0;
-	j = 0;
-	printf("ftstrlen:%d, k:%d\n", ft_strlen(str), k);
-	while (i < k)	
+	while (i < j)
 	{
-		if (tab[i] == tab[i + 1] - 1)
+		if (tab[i] == tab[i + 1] - 1 )
 			i++;
 		else
 		{
 			len++;
 			i++;
 		}
-		if (i  == k && tab[i - 1] + 1  != ft_strlen(str))
+		if (i == j && tab[i - 1] + 1 != ft_strlen(str))
 			len++;
 	}
-	printf("len:%d\n", len);
-	return (0);
+	return (len);
+}
 
+int	**ft_split(char *str, char *charset)
+{
+	int i;
+	int j;
+	int len;
+	char **strings;
+
+	len = ft_count_strings(str, charset);
+	strings = (char **)malloc(sizeof(char *) * len + 1);
+	i = 0;
+	j = 0;
+	while (*str != 0)
+	{
+		if (ft_check_charset(*str, charset))
+			str++;
+		else
+		{
+			i = 0;
+			len = 0;
+			while (!(ft_check_charset(str[i], charset))
+					&& str[i] != 0)
+			{
+				i++;
+				len++;
+			}
+			strings[j] = (char *)malloc(sizeof(char) * len + 1);
+			i = 0;
+			while (i < len)
+			{
+				strings[j][i] = *str;
+				str++;
+				i++;
+			}
+			j++;
+		}
+	}
+	strings[j] = 0;
+	i = 0;
+	while (strings[i] != 0)
+	{
+		printf("strings[%d]%s\n", i, strings[i]);
+		i++;
+	}
+	return (0);
 }
 
 int main()
 {
-	int **tab;
-	char charset[] = " ,";
-	char str[] = "Hello  ,World coucou, yo  y,   yy ,, test";
-	//char str2[] = "  ";
-	//int i;
-	//int j;
-
-	tab = ft_split(str, charset);//"Hello world, coucou", " ,"
+	char charset[] = "-+";
+	char str[] = "Hello-Yo-+Salit ca va?+ yep- yes-s-t-u";
+	ft_split(str, charset);//"Hello world, coucou", " ,"
 }
