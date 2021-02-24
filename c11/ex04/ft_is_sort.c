7cac394448e1ca0ca962cb7f0bd5700d0cc34bbd
Stone <stone@pop-os.localdomain>
2021-02-24 19:08:18 +0100

    corrected c11/ex04 error

--- c11/ex04/ft_is_sort.c
+++ c11/ex04/ft_is_sort.c
@@ -6,7 +6,7 @@
 /*   By: sdummett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/22 21:10:52 by sdummett          #+#    #+#             */
-/*   Updated: 2021/02/24 11:53:51 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/02/24 18:06:19 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@ int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 	int i;
 	int order;
 
-	if (length == 0)
-		return (0);
-	if (length == 1)
+	if (length == 0 || length == 1)
 		return (1);
 	i = 0;
 	while (f(tab[i], tab[i + 1]) == 0)
