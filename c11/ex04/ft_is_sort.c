2f9eb7f221dcd9b8268a33b15a5ae8a73468e888
Stone DUMMETT <sdummett@e3r6p8.42.fr>
2021-02-24 17:19:33 +0100

    lil modifs

--- c11/ex04/ft_is_sort.c
+++ c11/ex04/ft_is_sort.c
@@ -6,35 +6,34 @@
 /*   By: sdummett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/22 21:10:52 by sdummett          #+#    #+#             */
-/*   Updated: 2021/02/23 19:16:16 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/02/24 11:53:51 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 {
 	int i;
+	int order;
 
+	if (length == 0)
+		return (0);
+	if (length == 1)
+		return (1);
 	i = 0;
-	if (f(tab[i], tab[length - 1]) > 0)
-		while (i < length - 1)
-		{
-			if (f(tab[i], tab[i + 1]) < 0)
-				return (0);
-			i++;
-		}
-	else if (f(tab[i], tab[length - 1]) < 0)
-		while (i < length - 1)
-		{
-			if (f(tab[i], tab[i + 1]) > 0)
-				return (0);
-			i++;
-		}
+	while (f(tab[i], tab[i + 1]) == 0)
+		i++;
+	if (f(tab[i], tab[i + 1]) < 0)
+		order = 1;
 	else
-		while (i < length - 1)
-		{
-			if (f(tab[i], tab[i + 1]) != 0)
-				return (0);
+		order = -1;
+	while (tab[i] != 0 && i < length - 1)
+	{
+		if (f(tab[i], tab[i + 1]) < 0 && order == -1)
+			return (0);
+		else if (f(tab[i], tab[i + 1]) > 0 && order == 1)
+			return (0);
+		else
 			i++;
-		}
+	}
 	return (1);
 }
