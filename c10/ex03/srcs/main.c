e26b8285f54337df2a0689b61b5245b6b74f203b
Stone <stone.dummett@gmail.com>
2021-03-20 18:14:51 +0100

    redone from scratch the buffer_manager function, it only support 1 file with a maximum of 31 bytes

--- c10/ex03/srcs/main.c
+++ c10/ex03/srcs/main.c
@@ -6,83 +6,52 @@
 /*   By: sdummett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/19 01:20:44 by sdummett          #+#    #+#             */
-/*   Updated: 2021/03/20 16:49:36 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/03/20 18:03:18 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_hexdump.h"
 
-void	buffer_manager(char **filename, int size)
+void	buffer_manager(char **filename, int nboffiles)
 {
 	int		i;
 	int	 	j;
-	int		buf_size;
+	int		bufsize;
 	int		token;
 	long int	hex;
 	char		*buf;
-//	char		*buf_temp;
+//	char		*bytesToCmp_temp[33];
 
 	i = 1;
 	j = 0;
 	hex = 0;
 	token = 0;
 	buf = create_buf(filename[i]);
-	buf_size = compute_bufsize(filename[i]);
-	putaddr_inhex(hex);
-	ft_print_memory(buf, 16);
-	while (i < size)
+	bufsize = compute_bufsize(filename[i]);
+	if (i + 1 == nboffiles && bufsize < 32)
 	{
-		while (j < buf_size)
+		while (bufsize)
 		{
-			if (j + 32 <= buf_size)
+			if (bufsize > 16)
 			{
-				if (ft_memcmp(buf + j, buf + j + 16, 16) == 0)
-				{
-					if (token || (i == 1 && j == 0))
-					{
-						if (!(i == 1 && j == 0) || token)
-						{
-							putaddr_inhex(hex);
-							ft_print_memory(buf + j, 16);
-						}
-						ft_putstr("*\n");
-						token = 0;
-					}
-				}
-				else if (token == 1)
-				{
-					putaddr_inhex(hex);
-					ft_print_memory(buf + j, 16);
-				}
-				else
-				{
-					token = 1;
-				}
+				putaddr_inhex(hex);
+				ft_print_memory(buf + j, 16);
+				j = j + 16;
+				hex++;
 			}
-			else
+			else if (bufsize > 0)
 			{
-				if (i + 1 == size)
-				{
-					if (token == 1)
-					{
-						putaddr_inhex(hex);
-						ft_print_memory(buf + j, 16);
-						putaddr_inhex(hex + 1);
-						ft_print_memory(buf + j + 16, buf_size - j - 16);
-					}
-					else
-					{
-						putaddr_inhex(hex + 1);
-						ft_print_memory(buf + j + 16, buf_size - j - 16);
-					}
-					j = j + 16;
-				}
+				putaddr_inhex(hex);
+				ft_print_memory(buf + j, bufsize);
+				return ;
 			}
-			j = j + 16;
-			hex++;
+			bufsize = bufsize - 16;
 		}
-		i++;
 	}
+	//while (i < nboffiles)
+	//{
+
+	//}
 	free(buf);
 }
 
