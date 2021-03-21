04eb9f1d12a523153bac17c8bc90da9aa6c00a3a
Stone <stone.dummett@gmail.com>
2021-03-21 02:34:13 +0100

    ft_hexdump now concatenate different files & print one star if multiple lines is identical, it also prints the first and last line well

--- c10/ex03/srcs/main.c
+++ c10/ex03/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: sdummett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/19 01:20:44 by sdummett          #+#    #+#             */
-/*   Updated: 2021/03/21 02:00:52 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/03/21 02:28:37 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ void	buffer_manager(char **filename, int nfiles)
 	char	*buf;
 	int	currfile;
 	int	nbytes;
+	int	diff;
 	int	i;
 	int	j;
 
@@ -64,6 +65,7 @@ void	buffer_manager(char **filename, int nfiles)
 			}
 		}
 	}
+	diff = 0;
 	while (currfile < nfiles)
 	{
 		i = 0;
@@ -87,20 +89,32 @@ void	buffer_manager(char **filename, int nfiles)
 				}
 				else
 				{
+					if (diff == 0)
+					{
+						puthex(0);
+						ft_print_memory(tmp, 16);
+					}
 					puthex(0);
-					ft_print_memory(tmp, i);
+					ft_print_memory(tmp + 16, i);
 					return ;
 				}
 			}
 		}
 		if (ft_memcmp(tmp, tmp + 16, 16))
 		{
+			if (diff == 0)
+			{
+				puthex(0);
+				ft_print_memory(tmp, 16);
+			}
+			diff = 0;
+		}
+		else if (diff == 0)
+		{
 			puthex(0);
 			ft_print_memory(tmp, 16);
-		}
-		else
-		{
 			ft_putstr("*\n");
+			diff = 1;
 		}
 		shiftbytes(tmp);
 		//currfile++;
